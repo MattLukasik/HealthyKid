52145c20026889089ed4329cdbe3144c511a31fc
Mateusz Łukasik <manfred.mateo@wp.pl>
2019-11-25 19:33:01 +0100

    Healthy Kid config

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+
+import './Main.scss';
+
+class Main extends Component {
+  render() {
+    return <h1>Healthy Kid!</h1>;
+  }
+}
+
+export default Main;
